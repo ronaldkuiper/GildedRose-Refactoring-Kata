5703ff6b28579064c1d6d31e4138982fd0d544ff
Ronald Kuiper <ronald@newlin.nl>
2021-02-12 14:58:39 +0100

    Some refactoring

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,53 +2,37 @@ package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
 
+    companion object {
+        private const val item_concert = "Backstage passes to a TAFKAL80ETC concert"
+        private const val item_legendary = "Sulfuras, Hand of Ragnaros"
+        private const val item_brie = "Aged Brie"
+        private const val max_quality = 50
+        private const val min_quality = 0
+    }
+    
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+        for (item in items) {
+            if (item.name == item_legendary) {
+                continue
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+            val quality = when {
+
+                item.name == item_concert && item.sellIn < 6 -> item.quality + 3
+                item.name == item_concert && item.sellIn < 11 -> item.quality + 2
+                item.name == item_brie || item.name == item_concert -> item.quality + 1
+                else -> maxOf(min_quality, item.quality - 1)
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
+            item.quality = minOf(max_quality, quality)
+
+            item.sellIn = item.sellIn - 1
+
+            if (item.sellIn < 0) {
+                when {
+                    item.name == item_brie -> item.quality = minOf(max_quality, item.quality + 1)
+                    item.name == item_concert -> item.quality = min_quality
+                    item.quality > min_quality -> item.quality--
                 }
             }
         }
